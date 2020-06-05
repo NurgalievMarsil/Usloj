ff6b9dfae830b0c44ba16f967f37c925e744a217
NurgalievMarsil <ChechenskiyArc@yandex.ru>
2020-06-05 15:33:20 +0100

    fe

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 let num = 266219;
 num = String(num);
-let div = document.querySelector('.out');
-
 let prov = num[0] * num[1] * num[2] * num[3] * num[4] * num[5];
 prov = prov ** 3;
 prov = String(prov);
-div.innerHTML = prov.substr(0, 2);
+document.write(prov.substr(0, 2));
