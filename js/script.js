913117a2dc68623947c12b913e77424bec4bbd3e
NurgalievMarsil <ChechenskiyArc@yandex.ru>
2020-06-05 22:36:23 +0100

    f

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
-let num = 266219;
-num = String(num);
-let prov = num[0] * num[1] * num[2] * num[3] * num[4] * num[5];
+const num = 266219;
+const arr=[];
+const reducer = (accumulator, currentValue) => accumulator * currentValue;
+for(let temp = num;Math.round(temp)!=0;temp/=10,temp = Math.floor(temp)){
+  arr.unshift(temp%10);
+}
+let prov = (arr.reduce(reducer))
 prov = prov ** 3;
 prov = String(prov);
 document.write(prov.substr(0, 2));
