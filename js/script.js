f49c1b1fa223c8fe5c21c8f06f1cb669e51833a7
NurgalievMarsil <ChechenskiyArc@yandex.ru>
2020-06-17 16:19:51 +0100

    fe

--- js/script.js
+++ js/script.js
@@ -1,37 +1,13 @@
-let lang = 'ru';
-{
-  let arrRu = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
-  let arrEn = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
-  if (lang === 'ru') {
-      console.log(arrRu);
-  }
-  if (lang === 'en') {
-      console.log(arrEn);
-  }
-}
-{
-  let arrRu = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
-  let arrEn = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
-  switch (lang) {
-      case 'ru':
-          console.log(arrRu);
-          break;
-      case 'en':
-          console.log(arrEn);
-          break;
-  }
-}
-{
-  let arr = {
-      ru: ['пн', 'вт', 'ср','чт', 'пт', 'сб', 'вс'],
-      en: ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'],
-  };
-  console.log(arr[lang]);
-}
-let namePerson = prompt('Имя?', 'Артем');
-let message = (namePerson === 'Артем') ? 'Директор' : 
-  (namePerson === 'Максим') ? 'преподаватель' :
-  'студент';
-console.log(message);
+let arr = ['32', '27', '755', '104', '47', '78', '377']; // в виде строк
 
-
+arr.forEach((item) => {
+  if (item.startsWith('2') || item.startsWith('4')) {
+    console.log(item);
+  }
+});
+function isPrime(n) {
+  for (let i = 2; i * i <= n; i == 2 ? i++ : i += 2) if (n % i == 0) return false;
+  return n > 1;
+}
+const res = [...Array(101)].reduce((a, _, i) => a.concat(isPrime(i) ? `Делители числа ${i}: 1 и ${i}` : []) , []).join('\n');
+console.log(res);
