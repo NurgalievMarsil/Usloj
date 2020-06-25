a46168966f9af8f6ec84638139093b4f300465e5
NurgalievMarsil <ChechenskiyArc@yandex.ru>
2020-06-25 01:41:59 +0100

    xd

--- js/script.js
+++ js/script.js
@@ -1,13 +1,78 @@
-let arr = ['32', '27', '755', '104', '47', '78', '377']; // в виде строк
+"use strict";
 
-arr.forEach((item) => {
-  if (item.startsWith('2') || item.startsWith('4')) {
-    console.log(item);
-  }
+const todoControl = document.querySelector(".todo-control"),
+    headerInput = document.querySelector(".header-input"),
+    todoList = document.querySelector(".todo-list"),
+    todoCompleted = document.querySelector(".todo-completed");
+
+const addToStorage = function(obj) {
+    const todoJSON = JSON.stringify(obj);
+    
+    localStorage.setItem("list", todoJSON);
+};
+
+const readStorage = function() {
+    const returnObj = JSON.parse(localStorage.getItem("list"));
+
+    if (returnObj !== null) {
+        return returnObj;
+    } else {
+        return [];
+    }
+};
+
+const todoData = readStorage();
+
+const render = function() {
+    todoList.textContent = "";
+    todoCompleted.textContent = "";
+    addToStorage(todoData);
+
+    todoData.forEach(function(item) {
+        const li = document.createElement("li");
+        li.classList.add("todo-item");
+
+        li.innerHTML = "<span class=\"text-todo\">" + item.value + "</span>" +
+            "<div class=\"todo-buttons\">" + 
+                "<button class=\"todo-remove\"></button>" + 
+                "<button class=\"todo-complete\"></button>" + 
+            "</div>";
+
+        if (item.completed) {
+            todoCompleted.append(li);
+        } else {
+            todoList.append(li);
+        }
+        
+        const complete = li.querySelector(".todo-complete"),
+            remove = li.querySelector(".todo-remove");
+
+        complete.addEventListener("click", function() {
+            item.completed = !item.completed;
+            render();
+        });
+        
+        remove.addEventListener("click", function() {
+            todoData.splice(todoData.indexOf(item), 1);
+            render();
+        });
+    });
+};
+
+todoControl.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const newTodo = {
+        value: headerInput.value,
+        completed: false
+    };
+
+    if (newTodo.value.trim() !== "") {
+        todoData.push(newTodo);
+        headerInput.value = "";    
+    }
+    
+    render();
 });
-function isPrime(n) {
-  for (let i = 2; i * i <= n; i == 2 ? i++ : i += 2) if (n % i == 0) return false;
-  return n > 1;
-}
-const res = [...Array(101)].reduce((a, _, i) => a.concat(isPrime(i) ? `Делители числа ${i}: 1 и ${i}` : []) , []).join('\n');
-console.log(res);
+
+render();
